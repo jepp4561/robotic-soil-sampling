a166920d15c53a392b8742703f1092dcf9f80a38
Jeppe Hansen <jeppha@mmmi.sdu.dk>
2026-09-23 21:25:57 +0200

    Prepping for field day

--- src/soil_sampler_experiment/launch/experiment.launch.py
+++ src/soil_sampler_experiment/launch/experiment.launch.py
@@ -1,6 +1,10 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
@@ -31,7 +35,24 @@ def generate_launch_description():
         output="screen",
     )
 
+    dashboard = ExecuteProcess(
+        cmd=[
+            "bash",
+            "-c",
+            "source /opt/ros/jazzy/setup.bash && "
+            "source /home/$USER/ros_ws/mobile_robot_ws/install/setup.bash && "
+            "source /home/$USER/ros_ws/mobile_robot_ws/src/robotic-soil-sampling/.venv/bin/activate && "
+            "python -m soil_sampler_experiment.experiment_dashboard "
+            "--ros-args -r __node:=soil_sampler_experiment_dashboard",
+        ],
+        output="screen",
+    )
+
+
     return LaunchDescription([
         experiment_node,
-        visualization_node,
+        # visualization_node,
+        dashboard,
+
+        
     ])
